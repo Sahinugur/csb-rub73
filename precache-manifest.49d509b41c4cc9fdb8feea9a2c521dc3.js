35831c5f442db264a79a39e4b03684d32bea7236
Code Sandbox <hello@codesandbox.io>
2021-09-16 08:43:36 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.49d509b41c4cc9fdb8feea9a2c521dc3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9995791fd8a270681aed4dbf9080e03a",
+    "url": "https://sahinugur.github.io/csb-rub73/index.html"
+  },
+  {
+    "revision": "a48f9d490ac6c260b2fd",
+    "url": "https://sahinugur.github.io/csb-rub73/static/js/2.41d2c938.chunk.js"
+  },
+  {
+    "revision": "b955ba96adac7123f599",
+    "url": "https://sahinugur.github.io/csb-rub73/static/js/main.9d681663.chunk.js"
+  },
+  {
+    "revision": "9a883c3bb71c7b63816a",
+    "url": "https://sahinugur.github.io/csb-rub73/static/js/runtime-main.8096a2d6.js"
+  }
+]);
